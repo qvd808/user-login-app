54269d38f5d9fd94979eb145ce10ff7d790e622c
qvd808 <93557410+qvd808@users.noreply.github.com>
2022-02-23 15:07:39 -0800

    Hide db info using dotenv module

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,12 @@
+const config = require("dotenv").config()
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "",
-    password: "",
-    host: "",
-    port: "",
-    database: "",
+    user: config.parsed.DB_NAME,
+    password: config.parsed.DB_PASSWORD,
+    host: config.parsed.DB_HOST,
+    port: "5432",
+    database: config.parsed.DB_DEFAULT_DATABASE,
 });
 
 module.exports = pool;
